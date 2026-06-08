46794932f590be5dea81152dbe1ffa3f3ab11b54
Kyn <111687547+baekwun@users.noreply.github.com>
2026-06-08 09:27:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE = 'spendsense-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
